53dd8c00b86e90d7244f872128e883075a25f8c9
Jae <jhl038@ucsd.edu>
2015-08-12 20:57:05 -0700

    Updated Sign In Activity

--- app/src/main/java/com/jaelee213yahoo/shooon/MainActivity.java
+++ app/src/main/java/com/jaelee213yahoo/shooon/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signinscreen);
+        setContentView(R.layout.activity_main);
     }
 
     @Override
